fa2a8a8f95c5aa21e6a3391e612932833a12e05f
Osama Saleh <osama.saleh@mail.utoronto.ca>
2020-06-08 15:55:51 -0400

    Search bar formatted

---
+++ background.js
@@ -0,0 +1,17 @@
+
+console.log("Background Running");
+
+chrome.browserAction.onClicked.addListener(buttonClicked);
+
+function buttonClicked(tab){
+    let msg = {
+        txt: "hello"
+
+    }
+
+
+    chrome.tabs.sendMessage(tab.id);
+
+
+
+}
